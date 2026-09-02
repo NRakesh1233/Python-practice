d696e16d9585fa8c39273437505c829d824e3bb1
Rakesh-Nakka <rakesh6808123@gmail.com>
2026-09-02 23:39:53 +0530

    varables practice day 1

---
+++ day1.py
@@ -0,0 +1,40 @@
+
+
+
+
+# x=input("Enter your name: ")                                                                   
+
+# if x=="rakesh":
+#         print("Welcome rakesh!!")
+
+# x=int(input("Enter your name: "))
+# if x%2==0:
+#     print("even number")
+# else:
+#           print("odd number")
+
+
+print("india","pak", "usa",sep='-' )
+# x={1,2,3,4,4,5}
+
+# print(x)
+# y=2+3j      
+# print(y.real)
+# print(y.imag)
+# print(type(y))
+
+# a=111
+# print(a)
+
+# for i in range(1,11):
+#     print(i);
+# for i in [1,3,4,5,55,6,6,43,433]:
+#      print(i);  
+# for i in "rakesh":
+#      print(i)
+
+for i in {1,3,4,5,55,6,6,43,433}:
+     print(i)
+             
+                    
+                     
